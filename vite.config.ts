cc687e5fecbd8b69c3576599eefa70a236ba0217
Seva <seva@supercreator.app>
2024-10-26 21:05:34 +0300

    Refactor vite.config.ts and add updateManifestPlugin to plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json" assert { type: "json" }; // Node >=17
 import tsconfigPaths from "vite-tsconfig-paths";
+import { updateManifestPlugin } from "./updatemanifets";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), crx({ manifest })],
+  plugins: [tsconfigPaths(), crx({ manifest }),updateManifestPlugin()],
 });
